34ffc8b0cfabc803dc5a9c4366592c78e9757bc9
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-09-02 04:59:21 +0800

    Implementation of ReviewBomb Navigation Menu
    Frontend implementation of ReviewBomb navigation menu with both styling & mobile responsive implementation in desktop, mobile portrait, and mobile landscape views
    
    - JV

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      display: ['portrait', 'landscape'],
       colors: {
         primary: "#3B82F6",
         background: "#0F172A",
+        foundation: "#F8FAFC",
       },
       fontFamily: {
         'header': ['Poppins', 'sans-serif'],
