17f19e83a12afe16f2012ba872c421ce1627df1b
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-09-03 04:00:43 +0800

    JS Placeholder Implementation for Navigation Buttons
    Implementation of JS placeholder code functionality for top bombs, recent bombs, genre bombs, and all bombs buttons that is also dynamic handling different cases for top bombs, recent bombs, genre bombs, and all bombs buttons
    
    - JV

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,11 +1,81 @@
-import { showContentSections } from './utils.js';
+import * as utils from './utils.js';
 
 export function headerEventListeners() {
-    const reviewBombHeaderBtn = document.getElementById("reviewbomb-header-menubtn");
+    const topBombsBtn = document.querySelectorAll('.js-top-bombs-btn');
+    const recentBombsBtn = document.querySelectorAll('.js-recent-bombs-btn');
+    const genreBombsBtn = document.querySelectorAll('.js-genre-bombs-btn');
+    const allBombsBtn = document.querySelectorAll('.js-all-bombs-btn');
 
-    if (reviewBombHeaderBtn) {
-        reviewBombHeaderBtn.addEventListener('click', showContentSections);
-    } else {
-        console.error("ReviewBomb header button was not found!");
+    function handleTopBombsClick() {
+
+        const isDesktop = window.matchMedia('(min-width: 1024px)');
+        const isMobilePortrait = window.matchMedia('(max-width: 1023px) and (orientation: portrait)');
+        const isMobileLandscape = window.matchMedia('(max-width: 1023px) and (orientation: landscape)');
+
+        if (isDesktop.matches) {
+            utils.topBombsBtnDesktop();
+        } else if (isMobilePortrait.matches) {
+            utils.topBombsBtnMobilePortrait();
+        } else if (isMobileLandscape.matches) {
+            utils.topBombsBtnMobileLandscape();
+        }
     }
+
+    function handleRecentBombsClick() {
+        const isDesktop = window.matchMedia('(min-width: 1024px)');
+        const isMobilePortrait = window.matchMedia('(max-width: 1023px) and (orientation: portrait)');
+        const isMobileLandscape = window.matchMedia('(max-width: 1023px) and (orientation: landscape)');
+
+        if (isDesktop.matches) {
+            utils.recentBombsBtnDesktop();
+        } else if (isMobilePortrait.matches) {
+            utils.recentBombsBtnMobilePortrait();
+        } else if (isMobileLandscape.matches) {
+            utils.recentBombsBtnMobileLandscape();
+        }
+    }
+
+    function handleGenreBombsClick() {
+        const isDesktop = window.matchMedia('(min-width: 1024px)');
+        const isMobilePortrait = window.matchMedia('(max-width: 1023px) and (orientation: portrait)');
+        const isMobileLandscape = window.matchMedia('(max-width: 1023px) and (orientation: landscape)');
+
+        if (isDesktop.matches) {
+            utils.genreBombsBtnDesktop();
+        } else if (isMobilePortrait.matches) {
+            utils.genreBombsBtnMobilePortrait();
+        } else if (isMobileLandscape.matches) {
+            utils.genreBombsBtnMobileLandscape();
+        }
+    }
+
+    function handleAllBombsClick() {
+        const isDesktop = window.matchMedia('(min-width: 1024px)');
+        const isMobilePortrait = window.matchMedia('(max-width: 1023px) and (orientation: portrait)');
+        const isMobileLandscape = window.matchMedia('(max-width: 1023px) and (orientation: landscape)');
+
+        if (isDesktop.matches) {
+            utils.allBombsBtnDesktop();
+        } else if (isMobilePortrait.matches) {
+            utils.allBombsBtnMobilePortrait();
+        } else if (isMobileLandscape.matches) {
+            utils.allBombsBtnMobileLandscape();
+        }
+    }
+
+    topBombsBtn.forEach(button => {
+        button.addEventListener('click', handleTopBombsClick);
+    });
+
+    recentBombsBtn.forEach(button => {
+        button.addEventListener('click', handleRecentBombsClick);
+    })
+
+    genreBombsBtn.forEach(button => {
+        button.addEventListener('click', handleGenreBombsClick);
+    })
+
+    allBombsBtn.forEach(button => {
+        button.addEventListener('click', handleAllBombsClick);
+    })
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,52 @@
 export function showContentSections() {
-    console.log("Header Menu Button has been clicked!");
+    console.log("Header button has been clicked!");
+}
+
+export function topBombsBtnDesktop() {
+    console.log("Top Bombs Button in desktop view has been clicked!");
+}
+
+export function recentBombsBtnDesktop() {
+    console.log("Recent Bombs Button for the desktop view has been clicked!");
+}
+
+export function genreBombsBtnDesktop() {
+    console.log("Genre Bombs Button in desktop view has been clicked!");
+}
+
+export function allBombsBtnDesktop() {
+    console.log("All Bombs Button for the desktop view has been clicked!");
+}
+
+export function topBombsBtnMobilePortrait() {
+    console.log("Top Bombs Button in Mobile portrait view has been clicked!");
+}
+
+export function recentBombsBtnMobilePortrait() {
+    console.log("Recent Bombs Button in Mobile Portrait view has been clicked!");
+}
+
+export function genreBombsBtnMobilePortrait() {
+    console.log("Genre Bombs Button in Mobile Portrait view has been clicked!");
+}
+
+export function allBombsBtnMobilePortrait() {
+    console.log("All Bombs Button in Mobile Portrait view has been clicked!");
+}
+
+export function topBombsBtnMobileLandscape() {
+    console.log("Top Bombs Button in Mobile landscape view has been clicked!");
+}
+
+export function recentBombsBtnMobileLandscape() {
+    console.log("Recent Bombs Button in Mobile landscape view has been clicked!");
+}
+
+export function genreBombsBtnMobileLandscape() {
+    console.log("Genre Bombs Button in Mobile landscape view has been clicked!");
+}
+
+export function allBombsBtnMobileLandscape()
+{
+    console.log("All Bombs Button in Mobile landscape view has been clicked!");
 }
